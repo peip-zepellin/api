ca6a3d0acb16c68c7991f0b771b157282f17d27a
Mathis HOUIS <mathishouis@gmail.com>
2025-05-13 15:49:23 +0200

    Cleaned boot file

--- boot.py
+++ boot.py
@@ -8,39 +8,3 @@ import main
 
 main.start()
 
-
-"""
-
-from hcsr04 import HCSR04
-import time
-
-sensor = HCSR04(trigger_pin=5, echo_pin=18, echo_timeout_us=500*2*30) 
-
-# Seuil de distance en cm
-distance_threshold = 40
-while True:
-    # Mesure de la distance
-    distance = sensor.distance_cm()
-    print(distance, 'cm')
-    time.sleep(2)
-    
-"""
-    
-
-"""
-import machine
-from time import sleep
-
-gps_serial = machine.UART(2, baudrate=9600, tx=5, rx=18)
-
-while True:
-    print(gps_serial.any())
-    if gps_serial.any():
-        line = gps_serial.readline()  # Read a complete line from the UART
-        if line:
-            line = line.decode('utf-8')
-            print(line.strip())
-    sleep(0.5)
-    
-"""
-
